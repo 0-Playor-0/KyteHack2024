d6341e476b24d2d5b86043176f3c7166fc47f404
dsong7 <davidsongpike@gmail.com>
2024-06-08 21:29:06 -0400

    fix ml

--- root/backend/ml/sentiment-prediction.py
+++ root/backend/ml/sentiment-prediction.py
@@ -1,8 +1,14 @@
-import pickle
+import joblib
+import sklearn
+import os
+import json
+import sys
+
+print(os.getcwd())
 
 # Load the model
-model_file = open('sentiment-clf.pkl', 'rb')
-model = pickle.load(file)
+with open('sentiment_clf.pkl', 'rb') as model_file:
+    model = joblib.load(model_file)
 
 def predict(text):
     result = model.predict(text)
@@ -11,5 +17,5 @@ def predict(text):
 if __name__ == "__main__":
     input_data = json.loads(sys.stdin.read())
     text = input_data['text']
-    result = predict(text)
-    print(json.dumps({'prediction': result}))
+    result = predict([text])
+    print(json.dumps({'prediction': result[0]}))
