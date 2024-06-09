4a54852e64421773246daeb42b1751e6361b7640
circle-gon <97845741+circle-gon@users.noreply.github.com>
2024-06-09 10:49:02 +0000

    better website

--- root/backend/index.js
+++ root/backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import auth from "./routes/auth.js"
 import chat from "./routes/chat.js"
 import cookieParser from "cookie-parser"
+import { join } from "path"
 
 const app = express()
 
@@ -12,7 +13,29 @@ app.disable("x-powered-by")
 app.use(auth)
 app.use(chat)
 
-app.use(express.static("root/frontend"))
+function pathTo(file) {
+    return join(import.meta.dirname, `../frontend/${file}.html`)
+}
+
+app.get("/", (req, res) => {
+    if (!req.cookies.token) res.redirect("/login")
+    else res.sendFile(pathTo("index"))
+})
+
+app.get("/calendar", (req, res) => {
+    if (!req.cookies.token) res.redirect("/login")
+    else res.sendFile(pathTo("calendar"))
+})
+
+app.get("/login", (req, res) => {
+    if (req.cookies.token) res.redirect("/")
+    else res.sendFile(pathTo("login"))
+})
+
+app.get("/signup", (req, res) => {
+    if (req.cookies.token) res.redirect("/")
+    else res.sendFile(pathTo("signup"))
+})
 
 app.listen(3000, () => {
     console.log("Running!!!")
